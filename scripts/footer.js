5395d8ab39eaa1f4334b02dfa82327bc7bdba6e4
RobotArticulation <wigir@robotarticulation.com>
2018-12-02 20:47:23 +0100

    #2: first commit of website version 2

---
+++ scripts/footer.js
@@ -0,0 +1,77 @@
+function pageFooter(path) {
+
+$("div.page_footer").html(" \
+  <!-- BEGIN PRE-FOOTER --> \
+  <div class='pre-footer'> \
+    <div class='container'> \
+      <div class='row'> \
+        <!-- BEGIN BOTTOM CONTACTS --> \
+        <div class='col-md-3 col-sm-6'> \
+          <h2>Robot Articulation</h2> \
+          <address class='margin-bottom-20'> \
+            32 Rue Humbert<br> \
+            Le Bourg d'Oisans<br> \
+            38520, France<br> \
+            Email: <a href='mailto:info@robotarticulation.com'>info@robotarticulation.com</a><br> \
+          </address> \
+        </div> \
+        <!-- END BOTTOM CONTACTS --> \
+\
+        <!-- BEGIN BOTTOM ABOUT BLOCK --> \
+        <div class='col-md-3 col-sm-6 pre-footer-col'> \
+          <h2>About</h2> \
+          <p><a href='" + path + "/about.html'>About Us</a></p> \
+          <p><a href='" + path + "/contacts.html'>Contact Us</a></p> \
+        </div> \
+        <!-- END BOTTOM ABOUT BLOCK --> \
+\
+        <!-- BEGIN BOTTOM PRODUCTS BLOCK --> \
+        <div class='col-md-3 col-sm-6 pre-footer-col'> \
+          <h2>Products</h2> \
+            <p><a href='" + path + "/product_mercury_m30.html'>Mercury M30</a></p> \
+            <p><a href='" + path + "/product_mercury_m50.html'>Mercury M50</a></p> \
+            <p><a href='" + path + "/product_mercury_m65.html'>Mercury M65</a></p> \
+            <p><a href='" + path + "/product_mercury_link.html'>Mercury Link</a></p> \
+            <p><a href='" + path + "/product_mercury_power_hub.html'>Mercury Power Hub</a></p> \
+            <p><a href='" + path + "/product_key_features.html'>Key Features</a></p> \
+        </div> \
+        <!-- END BOTTOM PRODUCTS BLOCK --> \
+\
+        <!-- BEGIN BOTTOM DOWNLOADS BLOCK --> \
+        <div class='col-md-3 col-sm-6 pre-footer-col'> \
+          <h2>Downloads</h2> \
+          <p><a href='" + path + "/download_mercury_manuals.html'>Manuals</a></p> \
+          <p><a href='" + path + "/download_mercury_software.html'>Software</a></p> \
+          <p><a href='" + path + "/download_mercury_mechanical.html'>Mechanical Drawings</a></p> \
+        </div> \
+        <!-- END BOTTOM DOWNLOADS BLOCK --> \
+\
+      </div> \
+    </div> \
+  </div> \
+  <!-- END PRE-FOOTER --> \
+\
+  <!-- BEGIN FOOTER --> \
+  <div class='footer'> \
+    <div class='container'> \
+      <div class='row'> \
+        <!-- BEGIN COPYRIGHT --> \
+        <div class='col-md-4 col-sm-4 padding-top-10'> \
+          2017 © Robot Articulation. ALL Rights Reserved. \
+        </div> \
+        <!-- END COPYRIGHT --> \
+        <!-- BEGIN PAYMENTS --> \
+        <div class='col-md-4 col-sm-4'> \
+          <ul class='social-footer list-unstyled list-inline pull-right> \
+            <li><a href='https://www.linkedin.com/company/11211588' target='_blank'><i class='fa fa-linkedin'></i></a></li> \
+            <li><a href='https://github.com/RobotArticulation' target='_blank'><i class='fa fa-github'></i></a></li> \
+          </ul>  \
+        </div> \
+        <!-- END PAYMENTS --> \
+      </div> \
+    </div> \
+  </div> \
+  <!-- END FOOTER --> \
+  ");
+
+}
---
+++ scripts/header.js
@@ -0,0 +1,67 @@
+function pageHeader(path, img_path, active_menu) {
+
+$("div.page_header").html(" \
+  <div class='header'> \
+    <div class='container'> \
+       <div> \
+        <div class='ra-site-logo-wrapper col-xs-10 col-sm-11 col-md-11 col-lg-6'> \
+          <a class='site-logo' href='" + path + "/../index.html'> \
+            <img class='ra-header-img' src='" + img_path + "/img/logos/RobotArticulation_header_logo.png'></a> \
+        </div> \
+\
+        <div class='col-xs-2 col-sm-1 col-md-1 col-lg-6'> \
+          <a href='javascript:void(0);' class='mobi-toggler'><i class='fa fa-bars'></i></a> \
+        </div> \
+        <!-- BEGIN NAVIGATION --> \
+        <div class='header-navigation pull-right font-transform-inherit'> \
+          <ul> \
+            <li id='home'> \
+              <a href='" + path + "/../index.html'>Home</a> \
+            </li> \
+\
+            <li id='product' class='dropdown'> \
+              <a class='dropdown-toggle' data-toggle='dropdown' data-target='#' href='javascript:;'> \
+                Products \
+              </a> \
+\
+              <ul class='dropdown-menu'> \
+                <li><a href='" + path + "/product_mercury_m30.html'>Mercury M30</a></li> \
+                <li><a href='" + path + "/product_mercury_m50.html'>Mercury M50</a></li> \
+                <li><a href='" + path + "/product_mercury_m65.html'>Mercury M65</a></li> \
+                <li><a href='" + path + "/product_mercury_link.html'>Mercury Link</a></li> \
+                <li><a href='" + path + "/product_mercury_power_hub.html'>Mercury Power Hub</a></li> \
+                <li><a href='" + path + "/product_key_features.html'>Key Features</a></li> \
+              </ul> \
+            </li> \
+\
+            <li id='download' class='dropdown'> \
+              <a class='dropdown-toggle' data-toggle='dropdown' data-target='#' href='javascript:;'> \
+                Downloads \
+              </a> \
+\
+              <ul class='dropdown-menu'> \
+                <li><a href='" + path + "/download_mercury_manuals.html'>Manuals</a></li> \
+                <li><a href='" + path + "/download_mercury_software.html'>Software</a></li> \
+                <li><a href='" + path + "/download_mercury_mechanical.html'>Mechanical Drawings</a></li> \
+              </ul> \
+\
+            </li> \
+\
+            <li id='about'> \
+              <a href='" + path + "/about.html'>About us</a> \
+            </li> \
+\
+            <li id='contact'> \
+              <a href='" + path + "/contacts.html'>Contact us</a> \
+            </li> \
+          </ul> \
+        </div> \
+        <!-- END NAVIGATION --> \
+      </div> \
+    </div>  \
+  </div> \
+    ");
+
+  $( "#" + active_menu ).addClass("active");
+
+}
